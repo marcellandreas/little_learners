9a93050b895429faca473e07d83d25692633d5f9
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-04-18 18:34:59 +0700

    refactor: add gap

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -5,7 +5,7 @@ const MainLayout = ({ children }) => {
   return (
     <>
       <Navbar />
-      <main className=" container mx-auto my-3 grid grid-cols-12 px-5">
+      <main className=" container mx-auto my-3 grid grid-cols-12 px-5 pt-[50px] md:pt-[60px] lg:pt-[72px] lg:gap-y-52 md:gap-y-[150px] gap-y-20 lg:pb-[200px] md:pb-[150px] pb-20">
         {children}
       </main>
       <Footer />
