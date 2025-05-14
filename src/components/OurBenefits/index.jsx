6d8e161ec9c2ef5568bb8e092b88204f3649dea5
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-05-14 17:33:58 +0700

    feat: ini OurBenefits Component adding height for carBenefits

--- src/components/OurBenefits/index.jsx
+++ src/components/OurBenefits/index.jsx
@@ -47,13 +47,13 @@ const BenefitsData = [
 
 const CardBenefits = ({ id, icon, title, description }) => {
   return (
-    <section className="flex relative flex-col items-start self-stretch md:col-span-4 ">
+    <section className="flex relative flex-col min-h-36  items-start self-stretch md:col-span-4 ">
       <div className="flex pl-6 -top-7 gap-[10px] absolute">
         <div className="p-4 rounded-lg border-grey-15 bg-orange-90  border-2">
           <img src={icon} alt={`${id}-${title} icon`} className="w-6 h-6" />
         </div>
       </div>
-      <div className="flex pt-[50px] md:px-10 md:pt-[50px] md:pb-10 pb-[30px] pl-[30px] pr-[40px] flex-col gap-4 rounded-xl border-2 border-grey-15 shadow-[4px_4px_0px_1px_#1E1E1E]">
+      <div className="flex h-full pt-[50px] md:px-10 md:pt-[50px] md:pb-10 pb-[30px] pl-[30px] pr-[40px] flex-col gap-4 rounded-xl border-2 border-grey-15 shadow-[4px_4px_0px_1px_#1E1E1E]">
         <h3 className=" text-[22px] md:text-[24px] xl:text-[28px]  font-raleway text-grey-10 font-bold">
           {title}
         </h3>
