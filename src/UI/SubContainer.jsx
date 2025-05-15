a1ad006acf5b512f3399f98b1bb430a53fc4d411
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-05-15 09:21:32 +0700

    feat: adding w-full in subCOntainer

--- src/UI/SubContainer.jsx
+++ src/UI/SubContainer.jsx
@@ -2,7 +2,7 @@ import { SubTitle } from "./SubTitle";
 
 export const SubContainer = ({ heading, title, description }) => {
   return (
-    <div className="flex flex-col items-center gap-[10px] md:gap-[14px] 2xl:gap-5">
+    <div className="flex flex-col items-center gap-[10px] md:gap-[14px] 2xl:gap-5 w-full">
       <SubTitle>{heading}</SubTitle>
       <h2 className=" text-grey-10 text-center font-raleway text-4xl md:text-[48px] md:leading-[130%] font-bold">
         {title}
