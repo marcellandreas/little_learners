07a0c21220f6940d1b8a5bad0868c08b8150a093
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-05-20 21:22:22 +0700

    feat: menyesuaikan ContainerCard agar bisa digunakan di Contacts Page

--- src/components/Card/ContainerCard.jsx
+++ src/components/Card/ContainerCard.jsx
@@ -1,4 +1,17 @@
-const ContainerCard = ({ className, subtitle, title, description }) => {
+import {
+  ClockIcon,
+  EnvelopeIcon,
+  MapPinIcon,
+  PhoneIcon,
+} from "@heroicons/react/20/solid";
+
+const ContainerCard = ({
+  className,
+  subtitle,
+  title,
+  description,
+  isActive = true,
+}) => {
   return (
     <section
       className={`flex relative py-[50px] md:pt-[100px] 2xl:pt-[150px] 2xl:px-[120px] md:px-20 px-[30px] flex-col md:flex-row justify-center items-center gap-5 md:gap-[50px] rounded-xl border-2 border-grey-15 shadow-[6px_6px_0px_1px_#1E1E1E] bg-white w-full ${className}`}
@@ -16,9 +29,52 @@ const ContainerCard = ({ className, subtitle, title, description }) => {
           {title}
         </h2>
       </div>
-      <p className="text-grey-20 text-center font-outfit text-base 2xl:text-xl font-medium self-stretch md:flex-1">
-        {description}
-      </p>
+      {isActive ? (
+        <p className="text-grey-20 text-center font-outfit text-base 2xl:text-xl font-medium self-stretch md:flex-1">
+          {description}
+        </p>
+      ) : (
+        <div className="flex flex-col justify-center gap-[30px] md:flex-1">
+          <p className="text-grey-20 text-center font-outfit text-base 2xl:text-xl font-medium self-stretch md:flex-1">
+            {description}
+          </p>
+          {/* buttons */}
+          <div className="grid grid-cols-2 gap-5">
+            <button className="flex p-4 col-span-2 md:col-span-1 items-center gap-2 rounded-xl border-2 border-grey-15 bg-orange-99">
+              <div className="flex p-[6px] gap-[10px] rounded bg-orange-97 border-2 border-grey-15">
+                <EnvelopeIcon className="h-5 w-5" />
+              </div>
+              <p className="text-grey-20 text-center font-outfit text-base 2xl:text-xl font-medium">
+                hello@littlelearners.com
+              </p>
+            </button>
+            <button className="flex p-4 col-span-2 md:col-span-1 items-center gap-2 rounded-xl border-2 border-grey-15 bg-orange-99">
+              <div className="flex p-[6px] gap-[10px] rounded bg-orange-97 border-2 border-grey-15">
+                <PhoneIcon className="h-5 w-5" />
+              </div>
+              <p className="text-grey-20 text-center font-outfit text-base 2xl:text-xl font-medium">
+                +91 91813 23 2309
+              </p>
+            </button>
+            <button className="flex p-4 col-span-2 md:col-span-1 items-center gap-2 rounded-xl border-2 border-grey-15 bg-orange-99">
+              <div className="flex p-[6px] gap-[10px] rounded bg-orange-97 border-2 border-grey-15">
+                <MapPinIcon className="h-5 w-5" />
+              </div>
+              <p className="text-grey-20 text-center font-outfit text-base 2xl:text-xl font-medium">
+                Somewhere in the World
+              </p>
+            </button>
+            <button className="flex p-4 col-span-2 md:col-span-1 items-center gap-2 rounded-xl border-2 border-grey-15 bg-orange-99">
+              <div className="flex p-[6px] gap-[10px] rounded bg-orange-97 border-2 border-grey-15">
+                <ClockIcon className="h-5 w-5" />
+              </div>
+              <p className="text-grey-20 text-center font-outfit text-base 2xl:text-xl font-medium">
+                Office Hours - 9am - 6 pm
+              </p>
+            </button>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,9 +1,15 @@
+import ContainerCard from "../components/Card/ContainerCard";
 import MainLayout from "../Layout/MainLayout";
 
 const Contacts = () => {
   return (
     <MainLayout>
-      <div>Contacts</div>
+      <ContainerCard
+        title="Feel Free To Connect With Us"
+        description="We value open communication and are eager to assist you with any inquiries. Feel free to reach out to us through any of the following contact methods"
+        subtitle="Contact Us"
+        className="col-span-12"
+      />
     </MainLayout>
   );
 };
