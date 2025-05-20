0a0f7432c7c28ee59222e2e62a08138cca609539
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-05-20 22:53:35 +0700

    feat: selesai mengganti content pada bagian mission dan vision di about page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,14 @@
-const Card = () => {
+const Card = ({ title, icon, description }) => {
   return (
     <section className="flex p-10 md:p-[50px] 2xl:p-[60px] flex-col gap-10 md:gap-[50px] 2xl:gap-[60px] rounded-xl border-2 border-grey-15 bg-white shadow-[4px_4px_0px_1px_#1E1E1E]">
       <div className="flex items-center">
         <h3 className="flex-1 text-grey-10 font-raleway text-3xl md:text-4xl 2xl:text-[48px] 2xl:leading-[130%] font-bold">
-          Mission
+          {title}
         </h3>
-        <img src="/mission.svg" className=" h-auto w-auto" alt="" />
+        <img src={icon} className=" h-auto w-auto" alt={title} />
       </div>
       <p className="text-grey-20  font-outfit text-base 2xl:text-xl font-medium  ">
-        At Little Learners Academy, our mission is to inspire a passion for
-        learning and empower young minds to become confident, compassionate, and
-        creative individuals. We strive to create a safe and inclusive space
-        where children thrive academically, socially, and emotionally, setting
-        the stage for a successful educational journey.
+        {description}
       </p>
     </section>
   );
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -35,6 +35,23 @@ const AboutUs = () => {
       swiperInstance.navigation.update();
     }
   }, [swiperInstance, prevRef, nextRef]);
+
+  const dataMissionVision = [
+    {
+      id: 1,
+      title: "Mission",
+      description:
+        "At Little Learners Academy, our mission is to inspire a passion for learning and empower young minds to become confident, compassionate, and creative individuals. We strive to create a safe and inclusive space where children thrive academically, socially, and emotionally, setting the stage for a successful educational journey.",
+      icon: "/mission.svg",
+    },
+    {
+      id: 2,
+      title: "Vision",
+      description:
+        "Our vision is to be a beacon of educational excellence, where children are encouraged to explore, discover, and express their unique talents. We aim to foster a generation of lifelong learners equipped with critical thinking, empathy, and a deep appreciation for diversity.",
+      icon: "/vision.svg",
+    },
+  ];
   return (
     <MainLayout>
       <ContainerCard
@@ -52,8 +69,14 @@ const AboutUs = () => {
             growth."
         />
         <div className="flex flex-col md:flex-row md:gap-10 2xl:gap-[60px] md:justify-between gap-[30px]">
-          <Card />
-          <Card />
+          {dataMissionVision.map((data) => (
+            <Card
+              key={data.id}
+              icon={data.icon}
+              title={data.title}
+              description={data.description}
+            />
+          ))}
         </div>
       </section>
 
