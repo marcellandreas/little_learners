98a5ef4c80159e27259b9648eabf94f5164277d1
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-05-20 21:23:19 +0700

    feat: menambahkan isActive pada continerCard di COntanctPage

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -9,6 +9,7 @@ const Contacts = () => {
         description="We value open communication and are eager to assist you with any inquiries. Feel free to reach out to us through any of the following contact methods"
         subtitle="Contact Us"
         className="col-span-12"
+        isActive={false}
       />
     </MainLayout>
   );
